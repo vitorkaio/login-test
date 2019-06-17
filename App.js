64b9c8dc69e8d88ba3287562906dae6cd41d8447
vitorkaio <vitorcaiodepaula@gmail.com>
2019-06-17 14:08:56 -0300

    init

---
+++ App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import Login from 'pages/login/Login';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Login />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#1f2430',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
---
+++ src/pages/login/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+const Login = () => {
+  return (
+    <View>
+      <Icon name="person" size={40} color="#8a05be" />
+    </View>
+  );
+};
+
+export default Login;
